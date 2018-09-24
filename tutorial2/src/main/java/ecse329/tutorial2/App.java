5cdc5fe2bc91ff4aa210f958f2a0f256b5e7aa32
Bobo <bowen.tian@mail.mcgill.ca>
2018-09-24 14:10:08 -0400

    First commit

---
+++ tutorial2/src/main/java/ecse329/tutorial2/App.java
@@ -0,0 +1,13 @@
+package ecse329.tutorial2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
